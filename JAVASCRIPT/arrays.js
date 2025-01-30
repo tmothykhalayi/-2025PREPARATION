071baadeccab27a082107ec13f7198b1e642ca9a
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-01-30 12:17:32 +0300

    update

---
+++ JAVASCRIPT/arrays.js
@@ -0,0 +1,10 @@
+let fruits =["apple" ,"oranges" ,"lemons"]
+console.log (fruits);
+fruits.push("bananas");
+fruits.pop();
+fruits.unshift("pineapples");
+fruits.shift();
+let length = fruits.length ;
+let index =fruits.indexOf("apples");
+console.log(index);
+
---
+++ JAVASCRIPT/sorting.js
@@ -0,0 +1,5 @@
+let fruits =["apple" ,"oranges" ,"lemons"]
+for (let fruit of fruits){
+    console.log(fruits);
+}
+fruits = fruits.sort().reverse();
